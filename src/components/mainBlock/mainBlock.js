c3cdb4f65de5aba3a7cd16856cfcf2839727b8ba
haze-bear <connorrooney.ap@gmail.com>
2023-03-23 13:31:47 +0000

    padded out main block with filler

--- src/components/mainBlock/mainBlock.js
+++ src/components/mainBlock/mainBlock.js
@@ -4,7 +4,21 @@ import './mainBlock.css'
 function MainBlock() {
     return (
         <div className='mainBlock'>
-            <p>This is the Main Block</p>
+            <div className='titleHolder'>
+                <p>Welcome to my site!</p>
+            </div>
+            <div className='avatarHolder'></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sodales tellus turpis, eu volutpat neque rhoncus id. Maecenas molestie eget odio at venenatis.</p>
+
+            <p> Etiam vehicula velit ut metus finibus, sed maximus erat placerat. Sed in urna quis nulla pharetra ornare. Donec libero eros, eleifend in tempus eget, ultrices id mi. In vitae nulla bibendum orci mattis consectetur. Phasellus est felis, pretium at posuere eget, dignissim ac odio.</p> 
+
+            <p> Integer viverra vehicula erat a hendrerit. Aenean vel lorem magna. Ut dapibus mi eget magna euismod, vel pulvinar erat congue. Nunc et libero quis ipsum dapibus pellentesque vel eget odio. Sed iaculis diam non sapien commodo, ac dapibus lorem posuere. </p> 
+
+            <p>Sed vehicula dolor et lectus lacinia elementum. Vivamus congue convallis dolor id pharetra. Maecenas ut magna odio. Phasellus feugiat felis vel blandit mattis. In molestie lorem nec quam rutrum porta. Suspendisse vel ipsum dictum, porttitor elit ac, iaculis tellus. Donec vitae est facilisis, tempus augue in, tristique tortor. Duis nec dolor euismod, congue velit tincidunt, blandit sem. Vivamus congue urna non tortor tempus dapibus. </p> 
+
+           <p>Duis efficitur felis sed odio bibendum volutpat. Proin scelerisque augue bibendum consectetur tempor.</p>
+
+            <p>Proin placerat ipsum non tortor viverra, vitae molestie libero pellentesque.</p>
         </div>
     );
 };
